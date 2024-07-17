9bc1ad5d661df425223fae6b7f91d866e4147e25
Santiago Jimenez <santiagojim.salas@gmail.com>
2024-07-16 20:06:07 -0600

    Update Auth
    Auth complete redesign

--- frontend/stocker/src/screens/Auth.jsx
+++ frontend/stocker/src/screens/Auth.jsx
@@ -1,43 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Lock, Mail, User } from "lucide-react";
 
 function Auth() {
     return (
         <>
-            <div className="bg-st_gray flex justify-center items-center h-screen">
-                <div className="bg-white p-8 rounded shadow-md w-full max-w-sm">
-                    <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-                    <form>
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">Email</label>
-                            <input
-                                type="email"
-                                id="email"
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                placeholder="Enter your email" />
+            <div className="h-screen bg-imgModule bg-cover flex justify-center items-center">
+                <section className="size-8/12 bg-white rounded-lg grid grid-cols-2 divide-x-2 divide-gray divide-opacity-20">
+
+                    <div className="flex flex-col justify-between items-center p-10">
+                        <h1 className="text-4xl">CREAR CUENTA</h1>
+                        <div className="flex items-center w-full space-x-2  border-2 border-st_gray rounded-lg px-3">
+                            <User />
+                            <span className="h-[90%] w-[2px] rounded-full bg-st_gray"></span>
+                            <input type="text" className="w-full h-14" placeholder="Nombre del administrador" />
                         </div>
-                        <div className="mb-6">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                            <input
-                                type="password"
-                                id="password"
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                                placeholder="Enter your password" />
+                        <div className="flex items-center w-full space-x-2  border-2 border-st_gray rounded-lg px-3">
+                            <Mail />
+                            <span className="h-[90%] w-[2px] rounded-full bg-st_gray"></span>
+                            <input className="w-full h-14" type="email" placeholder="Correo electronico" />
                         </div>
-                        <div className="flex items-center justify-between">
-                            <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                type="button">
-                                <Link to="/wbA">Login</Link>
-                            </button>
-                            <a
-                                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                                href="#">
-                                Forgot Password?
-                            </a>
+                        <div className="flex items-center w-full space-x-2  border-2 border-st_gray rounded-lg px-3">
+                            <Lock />
+                            <span className="h-[90%] w-[2px] rounded-full bg-st_gray"></span>
+                            <input className="w-full h-14 border-st_gray " type="password" placeholder="Contraseña" />
                         </div>
-                    </form>
-                </div>
+                        <div className="flex items-center w-full space-x-2  border-2 border-st_gray rounded-lg px-3">
+                            <Lock />
+                            <span className="h-[90%] w-[2px] rounded-full bg-st_gray"></span>
+                            <input className="w-full h-14 border-st_gray " type="password" placeholder="Confirmar contraseña" />
+                        </div>
+                        <button className="w-[80%] p-4 rounded-xl bg-st_blue text-white text-2xl">Crear Cuenta</button>
+                    </div>
+
+                    <div className="flex flex-col justify-between items-center p-10">
+                        <h1 className="text-4xl">INICIAR SESION</h1>
+                        <div className="flex items-center w-full space-x-2  border-2 border-st_gray rounded-lg px-3">
+                            <Mail />
+                            <span className="h-[90%] w-[2px] rounded-full bg-st_gray"></span>
+                            <input className="w-full h-14" type="email" placeholder="Correo electronico" />
+                        </div>
+                        <div className="flex items-center w-full space-x-2  border-2 border-st_gray rounded-lg px-3">
+                            <Lock />
+                            <span className="h-[90%] w-[2px] rounded-full bg-st_gray"></span>
+                            <input className="w-full h-14 border-st_gray " type="password" placeholder="Contraseña" />
+                        </div>
+                        <button className="w-[80%] p-4 rounded-xl bg-st_blue text-white text-2xl"><Link to="/wba">Iniciar Sesion</Link></button>
+                    </div>
+
+                </section>
             </div>
         </>
     );
